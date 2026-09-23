d22e0c27ce82d36304e1a14e9bf91a688175c69e
carlmoya <camoyaastudillo@my.waketech.edu>
2026-09-22 22:02:55 -0400

    Overhauled hover script
    Hover script now supports hovering along the X and the Y axis.

--- Adventure/Assets/Scripts/Hover.cs
+++ Adventure/Assets/Scripts/Hover.cs
@@ -6,23 +6,50 @@ public class Hover : MonoBehaviour
 {
     // Fields
 
-    public float frequency = 1f;
-    public float amplitude = 1f;
+    [Header("Hover Settings")]
+    public Vector2 frequencies = Vector2.one;
+    public Vector2 amplitudes = Vector2.one;
 
-    private float startY;
+    [Header("Update Settings")]
+    public bool useUnscaledDeltaTime = false;
+
+    private Vector2 midline;
     private float elapsedTime = 0f;
 
     // Methods
 
     private void Start()
     {
-        startY = transform.localPosition.y;
+        // Set midline
+        midline = transform.localPosition;
     }
 
     private void Update()
     {
-        elapsedTime += Time.deltaTime;
+        // Increase the elapsed time
+        elapsedTime += useUnscaledDeltaTime ? Time.unscaledDeltaTime : Time.deltaTime;
 
-        transform.localPosition = new Vector2(transform.localPosition.x, startY + Mathf.Sin(elapsedTime * frequency) * amplitude);
+        // Set the current position
+        transform.localPosition = TargetPosition();
+    }
+
+    // Return Methods
+
+    private Vector2 TargetPosition()
+    {
+        // Get the target x position
+        float targetX = midline.x + SinePoint(amplitudes.x, frequencies.x);
+
+        // Get the target y position
+        float targetY = midline.y + SinePoint(amplitudes.y, frequencies.y);
+
+        // Return the target position
+        return new Vector2(targetX, targetY);
+    }
+
+    private float SinePoint(float amplitude, float frequency)
+    {
+        // Return a point on a sine wave
+        return amplitude * Mathf.Sin(elapsedTime * frequency);
     }
 }
